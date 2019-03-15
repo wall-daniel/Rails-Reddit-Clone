f93946ccc3dfea4465a6b435d2b788b1ef5b1737
wall-daniel <dannywall@cmail.carleton.ca>
2019-03-15 19:12:39 -0400

    implement basic login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.subreddit_id = params[:subreddit_id]
+    @post.user_id = current_user.id
     if @post.save
       redirect_to Subreddit.find(params[:subreddit_id])
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
 
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get 'users/new'
   get 'users/show'
   get 'users/index'
